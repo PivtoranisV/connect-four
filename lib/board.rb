c2c985bad7d53682acabc757b0be9d8b7a456f39
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-13 23:20:42 +0300

    Add method to create grid

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class Board
+  attr_reader :grid
+
+  def initialize
+    @grid = create_grid
+  end
+
+  private
+
+  def create_grid(rows = 6, columns = 7)
+    Array.new(rows) { Array.new(columns) }
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,8 +4,11 @@ require_relative '../lib/board'
 
 describe Board do
   subject(:board) { described_class.new }
-  it 'Creates an empty board with 7 columns and 6 rows' do
-    expect(board.create_grid.length).to eq(6)
-    expect(board.create_grid.all? { |row| row.length == 7 }).to be true
+
+  describe '#initialize' do
+    it 'Creates an empty board with 7 columns and 6 rows' do
+      expect(board.grid.length).to eq(6)
+      expect(board.grid.all? { |row| row.length == 7 }).to be true
+    end
   end
 end
