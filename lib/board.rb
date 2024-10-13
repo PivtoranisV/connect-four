c527d26277407f5b67df6f70e391468360c57700
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-14 01:14:05 +0300

    Add method what return true if 4 same pieces consecutively in a row

--- lib/board.rb
+++ lib/board.rb
@@ -14,6 +14,19 @@ class Board
     grid[row][column] = piece
   end
 
+  def game_over?
+    winning_combination = [
+      # rows
+      grid[0], grid[1], grid[2], grid[3], grid[4], grid[5]
+    ]
+    winning_combination.each do |combo|
+      combo.each_cons(4) do |four_cell|
+        return true if four_cell.uniq.size == 1 && !four_cell.include?(nil)
+      end
+    end
+    false
+  end
+
   private
 
   def create_grid(rows = 6, columns = 7)
