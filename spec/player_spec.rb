fa0c92bae1a1dff9ce005ae7668e7eef6e248a14
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-14 12:48:29 +0300

    Add test to validate make_move method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,4 +10,14 @@ describe Player do
       expect(player.piece).to eq('X')
     end
   end
+
+  describe '#make_move' do
+    context 'when valid move is entered' do
+      it 'returns the chosen column number' do
+        allow(player).to receive(:gets).and_return('1')
+
+        expect(player.make_move).to eq('1')
+      end
+    end
+  end
 end
