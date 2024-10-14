7179156afc79cc4072a93d75c9c76525875b3446
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-14 10:23:59 +0300

    Refactor game_over, getting rows and column dynamically

--- lib/board.rb
+++ lib/board.rb
@@ -15,20 +15,14 @@ class Board
   end
 
   def game_over?
-    winning_combination = [
-      # rows
-      grid[0], grid[1], grid[2], grid[3], grid[4], grid[5],
-      # columns
-      [grid[0][0], grid[1][0], grid[2][0], grid[3][0], grid[4][0], grid[5][0]],
-      [grid[0][1], grid[1][1], grid[2][1], grid[3][1], grid[4][1], grid[5][1]],
-      [grid[0][2], grid[1][2], grid[2][2], grid[3][2], grid[4][2], grid[5][2]],
-      [grid[0][3], grid[1][3], grid[2][3], grid[3][3], grid[4][3], grid[5][3]],
-      [grid[0][4], grid[1][4], grid[2][4], grid[3][4], grid[4][4], grid[5][4]],
-      [grid[0][5], grid[1][5], grid[2][5], grid[3][5], grid[4][5], grid[5][5]],
-      [grid[0][6], grid[1][6], grid[2][6], grid[3][6], grid[4][6], grid[5][6]]
-    ]
-    return true if four_in_line(winning_combination)
-
+    #  Row
+    grid.each do |row|
+      return true if four_in_line?(row)
+    end
+    # Column
+    grid.transpose.each do |column|
+      return true if four_in_line?(column)
+    end
     false
   end
 
@@ -42,11 +36,9 @@ class Board
     (grid.length - 1).downto(0).find { |row| grid[row][column].nil? }
   end
 
-  def four_in_line(line)
-    line.each do |combo|
-      combo.each_cons(4) do |four_cell|
-        return true if four_cell.uniq.size == 1 && !four_cell.include?(nil)
-      end
+  def four_in_line?(line)
+    line.each_cons(4) do |four_cell|
+      return true if four_cell.uniq.size == 1 && !four_cell.include?(nil)
     end
     false
   end
