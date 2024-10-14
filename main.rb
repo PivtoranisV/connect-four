e032a52d8169534935739209b0fb1fa93ae80b1c
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-14 09:59:20 +0300

    Add method what return true if 4 same pieces consecutively in a column

--- main.rb
+++ main.rb
@@ -4,10 +4,10 @@ require_relative 'lib/board'
 require_relative 'lib/game'
 
 game = Board.new
-game.add_piece(0, 'X')
 game.add_piece(1, 'X')
-game.add_piece(2, 'X')
-game.add_piece(3, 'X')
-game.add_piece(4, 'P')
+game.add_piece(1, 'X')
+game.add_piece(1, 'X')
+game.add_piece(1, 'X')
+game.add_piece(1, 'P')
 p game.game_over?
 p game.grid
