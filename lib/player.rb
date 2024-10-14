e6f97ec51cf5e28631f662a017357f384781c6ae
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-14 14:07:12 +0300

    Refactor move validation functionality

--- lib/player.rb
+++ lib/player.rb
@@ -13,7 +13,15 @@ class Player
     puts 'Please enter a number of column from 1 to 7 to identify your option:'
     loop do
       answer = gets.chomp
-      return answer if answer.match?(/^[1-7]$/)
+      return answer if valid_input(answer)
+
+      puts 'Invalid option. The input should be a number between 1-7.'
     end
+  end
+
+  private
+
+  def valid_input(input)
+    input.match?(/^[1-7]$/)
   end
 end
--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 
 require_relative 'lib/board'
 require_relative 'lib/game'
+require_relative 'lib/player'
 
 game = Board.new
 game.add_piece(0, 'X')
@@ -16,3 +17,6 @@ game.add_piece(3, '0')
 game.add_piece(3, 'X')
 p game.game_over?
 game.grid.each { |row| p row }
+
+player = Player.new('Bob', 'X')
+player.make_move
